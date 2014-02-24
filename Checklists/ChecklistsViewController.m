98d1fd85c2157404bf734fe4ed1de97756fca465
Derek Bassett <pabassett@gmail.com>
2014-02-23 23:30:13 -0700

    01 Table View

--- Checklists/ChecklistsViewController.m
+++ Checklists/ChecklistsViewController.m
@@ -26,4 +26,43 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 100;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+    cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ChecklistItem"];
+    UILabel *label = (UILabel*)[cell viewWithTag:1000];
+    
+    if(indexPath.row % 5 == 0) {
+        label.text = @"Walk the dog";
+    } else if (indexPath.row % 5 == 1) {
+        label.text = @"Brush my teeth";
+    } else if (indexPath.row % 5 == 2) {
+        label.text = @"Learn iOS development";
+    } else if (indexPath.row % 5 == 3) {
+        label.text = @"Soccer practice";
+    } else if (indexPath.row % 5 == 4) {
+        label.text = @"Eat ice cream";
+    }
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    
+    if(cell.accessoryType == UITableViewCellAccessoryNone) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    } else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
 @end
