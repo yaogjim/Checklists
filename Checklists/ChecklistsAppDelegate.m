976520a472408275e2f374f4c5a5d7344d8479cf
Derek Bassett <pabassett@gmail.com>
2014-02-28 11:21:52 -0700

    Add debug for notifications.

--- Checklists/ChecklistsAppDelegate.m
+++ Checklists/ChecklistsAppDelegate.m
@@ -61,4 +61,9 @@
     [self saveData];
 }
 
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
+{
+    NSLog(@"didReceiveLocalNotification %@", notification);
+}
+
 @end
